b0d4ea85d1715c46fd5b3d89aff3bddaad70860c
Gretel <furrergretel@gmail.com>
2025-09-02 18:23:39 -0600

    Add data folder

---
+++ Datathon WiDS 2024 Challenge.py
@@ -0,0 +1,27 @@
+#Datathon WiDS 2024 Challenge
+
+#%% Importación de librerías
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import os
+import warnings
+
+# Scikit-Learn
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_validate
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, accuracy_score, roc_auc_score, ConfusionMatrixDisplay
+from sklearn.feature_selection import RFECV
+from sklearn.model_selection import cross_val_score
+
+# %%
+df_datathon = pd.read_csv('/Users/gretelfurrer/MAESTRIA_CIENCIA_DE_DATOS/3rd_sem_MCD/GRANDES_BASES_DE_DATOS_II/widsdatathon2024-challenge1/training.csv', sep=',') 
+df_datathon.head()
+# %%
+df_datathon.info()
+# %%
